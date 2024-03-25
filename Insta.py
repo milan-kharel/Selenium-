0aa9bcadc2056f3c8c94a172bdfc6e91cd04f1d0
milan-kharel <milan.kharel120@gmail.com>
2024-03-25 14:10:03 +0545

    open youtube.com find search box enter the key new movie trailer and search it and show result

--- Insta.py
+++ Insta.py
@@ -2,7 +2,12 @@ from time import sleep
 from selenium import webdriver
 
 browser = webdriver.Chrome()
+browser.implicitly_wait(10)
+
 browser.get('https://www.instagram.com/')
+
+login_link = browser.find_element_by_xpath("//a[text()='Log in']")
+login_link.click()
 
 sleep(10)
 
---
+++ Youtube.py
@@ -0,0 +1,20 @@
+from time import sleep  # Used to pause the program execution
+from selenium import webdriver  # Used to automate browser interaction
+from selenium.webdriver.common.by import By  # Used to locate elements on the web page
+from selenium.webdriver.common.keys import Keys
+
+
+browser = webdriver.Chrome()
+
+browser.get('http://www.Youtube.com')
+
+# Find the search box on the page using its name attribute
+search_box = browser.find_element(By.NAME, 'search_query')
+
+# Type "seleniumhq" in the search box and press Enter
+search_box.send_keys('new movie trailer') #use any keyword to search or browse the youtube
+search_box.send_keys(Keys.RETURN)
+
+sleep(10) 
+
+browser.quit()
